ba7ea0c687765915cac294b9ea0b48d1f8e183ad
jamiesaunders03 <58827706+jamiesaunders03@users.noreply.github.com>
2020-04-30 19:41:38 +0100

    Add files via upload

--- cogs/diceroller.py
+++ cogs/diceroller.py
@@ -1,5 +1,8 @@
-from discord.ext import commands
+import random
+import re
+
 import discord
+from discord.ext import commands
 
 
 class DiceRoller(commands.Cog):
@@ -8,8 +11,7 @@ class DiceRoller(commands.Cog):
 
     @commands.command()
     async def roll(self, ctx, dice_str: str):
-        """ Roll a set of dice e.g 4d6kh3"""
-        pass
+        await ctx.send(Roll.roll(dice_str), embed=None)
 
     @commands.command()
     async def randstats(self, ctx):
@@ -20,6 +22,190 @@ class DiceRoller(commands.Cog):
     async def charroll(self, ctx, dice_str: str):
         """ Roll a set of dice """
         pass
+
+
+class Roll:
+
+    @classmethod
+    def roll(cls, command: str):
+        command = Roll.format(command)
+        if type(command) == str:
+            return command
+
+        total = 0
+        throws = ""
+
+        for throw in command:
+            neg = -1 if (throw[0] == "-") else 1
+            rolls, dice_meta = cls.setup_command(throw.replace("-", ""))
+
+            try:
+                roll = RandDice(**dice_meta) * rolls
+            except ValueError as e:
+                return e
+
+            total += sum(roll) * neg
+            throws += f"{throw}: {' '.join([str(num) for num in roll])}\n"
+
+        throws += f"Total: {total}"
+
+        return throws
+
+    @classmethod
+    def setup_command(cls, dice_throw: str):
+        rolls, dice_meta = dice_throw.split("d")
+        rolls = int(rolls) if rolls != "" else 1
+
+        sides = int(dice_meta[:Utilities.index_next_int(dice_meta)])
+        dice_meta = dice_meta[Utilities.index_next_int(dice_meta):]
+        kh, kl, exp = cls.get_dice_modifiers(dice_meta)
+
+        throws_meta = {"dice_sides": sides, "kh": kh, "kl": kl, "exp": exp}
+
+        return rolls, throws_meta
+
+    @staticmethod
+    def format(command):
+        command = command.lower()
+        state_1 = re.compile("-?\d*d\d+(k[hl]\d+)?(e\d+)?").search
+        state_2 = re.compile("-?\d*d\d+(e\d+)?(k[hl]\d+)?").search
+        invalid = re.compile("(d\D|^-\d$)").findall
+        command = command.replace("-", "+-").replace(" ", "").split("+")
+        command_parts = []
+        for dice_roll in command:
+            if not invalid(dice_roll) and len(dice_roll) > 1:
+                option_1 = state_1(dice_roll).group()
+                option_2 = state_2(dice_roll).group()
+                if dice_roll == option_1 or dice_roll == option_2:
+                    command_parts.append(dice_roll)
+
+        if command_parts == command:
+            return command_parts
+        else:
+            command = "+".join(command).replace("+-", "-")
+            command_parts = "+".join(command_parts).replace("+-", "-")
+            return Utilities.show_error(command, command_parts)
+
+    @staticmethod
+    def get_dice_modifiers(meta: str):
+        kh = kl = exp = None
+        if meta:
+            if "e" in meta:
+                split = meta.replace("kl", "kh").split("kh")
+                split = [item for item in split if item]
+                split = [item for item in split if not item.isdigit()][0]
+                while not split.isdigit():
+                    split = split[1:]
+                exp = int(split)
+
+            if "k" in meta:
+                split = meta.split("e")
+                split = [item for item in split if item]
+                split = [item for item in split if not item.isdigit()][0]
+                while split[0] not in "hl":
+                    split = split[1:]
+
+                if split[0] == "l":
+                    kl = int(split[1:])
+                else:
+                    kh = int(split[1:])
+
+        return kh, kl, exp
+
+
+class Utilities:
+
+    @classmethod
+    def difference(cls, str_1, str_2):
+        while len(str_1) != 0 and len(str_2) != 0:
+            if str_1[0] == str_2[0]:
+                str_1 = str_1[1:]
+                str_2 = str_2[1:]
+            else:
+                return cls.string_lengths(str_1, str_2)
+
+        return cls.string_lengths(str_1, str_2)
+
+    @staticmethod
+    def string_lengths(*strings: str, order="desc") -> str:
+        reverse = (order == "desc")
+        return sorted(strings, key=len, reverse=reverse)[0]
+
+    @staticmethod
+    def index_substring(string, sub_string):
+        if sub_string not in string:
+            return None
+        else:
+            sub_length = len(sub_string)
+            index = 0
+            while sub_string not in string[index: index + sub_length + 1]:
+                index += 1
+
+            return index + 1
+
+    @staticmethod
+    def index_next_int(string: str):
+        for count, char in enumerate(string):
+            if not char.isdigit():
+                return count
+            count += 1
+
+        return len(string)
+
+    @classmethod
+    def show_error(cls, string, sub_string):
+        index = cls.index_substring(string, cls.difference(string, sub_string))
+        error_text = f"{string}\n{'-'*index}^ error near here\n"
+
+        return error_text
+
+
+class RandDice:
+
+    max_rolls = 100
+
+    def __init__(self, dice_sides: int, kh: int = None, kl: int = None, exp: int = None):
+        self.sides = dice_sides
+        self.kh = kh
+        self.kl = kl
+        self.e = exp
+
+        if kh and kl:
+            raise ValueError("KH and KL cannot be specified on the same throw")
+
+        if dice_sides == 1 and exp == 1:
+            raise ValueError("Hold on there, you can't just go around throwing infinite dice like you own the place")
+
+        if self.kh is None:
+            self.kh = -1
+        if self. kl is None:
+            self.kl = -1
+        if self.e is None:
+            self.e = -1
+
+        if self.kh < 0 and self.kh != -1 or self.kl < 0 and self.kl != -1:
+            raise ValueError("I'm sorry but you can't keep a negative number of dice, that just doesn't work does it")
+
+    def __mul__(self, other):
+        if other > self.max_rolls:
+            raise ValueError("Sorry but you can only roll a dice up to 100 times, "
+                             "what do you want a number that big for anyways")
+
+        output = []
+        for _ in range(other):
+            roll = random.randint(1, self.sides)
+            output.append(roll)
+
+        output = sorted(output)
+        if self.kh != -1:
+            output = output[-self.kh:]
+        elif self.kl != -1:
+            output = output[:self.kl]
+
+        for _ in range(len([num for num in output if num == self.e])):
+            output += self * 1
+
+        return output
 
 
 def setup(bot):
