60dea4a267033774b6095c4f977f03548b3e8881
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-05-03 16:33:22 +0100

    yeet

--- cogs/audio.py
+++ cogs/audio.py
@@ -357,7 +357,7 @@ class Audio(commands.Cog):
 
         if not hasattr(bot, 'lavalink'):  # This ensures the client isn't overwritten during cog reloads.
             bot.lavalink = lavalink.Client(641381762785607698)
-            bot.lavalink.add_node('209.126.3.1', 2333, 'youshallnotpass', 'eu', 'default-node')  # Host, Port, Password, Region, Name
+            bot.lavalink.add_node('127.0.0.1', 2333, 'youshallnotpass', 'eu', 'default-node')  # Host, Port, Password, Region, Name
             bot.add_listener(bot.lavalink.voice_update_handler, 'on_socket_response')
 
         lavalink.add_event_hook(self.track_hook)
