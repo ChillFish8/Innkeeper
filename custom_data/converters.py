7afcb930673aa791732603e6f6c1fa7b853dab9e
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-05-22 12:52:35 +0100

    #3 - Custom spells

--- custom_data/converters.py
+++ custom_data/converters.py
@@ -4,6 +4,7 @@ import pymongo
 import json
 import logging
 
+
 class Settings:
     @classmethod
     def get_config_default(cls) -> dict:
@@ -72,7 +73,8 @@ class MongoDatabase:
     # Spells
     def add_user_spells(self, user_id: int, name: str, url: str) -> [dict, int]:
         current_data = self.user_spells.find_one({'_id': user_id})
-        logging.log(logging.DEBUG, "User with Id: {} returned with results: {}".format(user_id, current_data))
+        logging.log(logging.DEBUG, "ADD-SPELLS: User with Id: {} returned with results: {}".format(
+            user_id, current_data))
         if current_data is not None:
             urls = current_data['urls']
             urls.append({'name': name, 'url': url})
@@ -84,12 +86,33 @@ class MongoDatabase:
             data = {'_id': user_id, 'urls': [{'name': name, 'url': url}]}
             resp = self.user_spells.insert_one(data)
             logging.log(logging.INFO, "Data inserted into area with UserId: {},\n"
-                                      "      resp: {}\n".format(user_id, {'_id': resp.inserted_id, 'complete': resp.acknowledged}))
+                                      "      resp: {}\n".format(user_id,
+                                                                {'_id': resp.inserted_id,
+                                                                 'complete': resp.acknowledged}))
             return resp.inserted_id
 
+    def remove_user_spells(self, user_id: int, name: str):
+        def check(value):
+            return not value['name'] == name
 
+        current_data = self.user_spells.find_one({'_id': user_id})
+        logging.log(logging.DEBUG,
+                    "DELETE-SPELLS: User with Id: {} returned with results: {}".format(user_id, current_data))
+        if current_data is not None:
+            urls = current_data['urls']
+            urls = list(filter(check, urls))
+            resp = self.user_spells.update_one({'_id': user_id}, {'$set': {'urls': urls}})
+            logging.log(logging.INFO, "Data updated area with UserId: {},\n"
+                                      "      resp: {}\n".format(user_id, resp.raw_result))
+            return resp.raw_result
+        else:
+            return "NO-SPELLS"
 
-
+    def get_user_spells(self, user_id: int) -> dict:
+        current_data = self.user_spells.find_one({'_id': user_id})
+        logging.log(logging.DEBUG, "GET-SPELLS: User with Id: {} returned with results: {}".format(user_id,
+                                                                                                   current_data))
+        return current_data
 
 
 class CustomSpells:
@@ -102,4 +125,3 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     db = MongoDatabase()
     print(db.add_user_spells(12345, "https://helpme.com/", "fireball"))
-
