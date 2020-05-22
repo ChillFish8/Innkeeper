b73ad4d13c1d6ff1772d15b7e4072144e4d271d3
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-05-22 12:36:00 +0100

    #1 - Custom guild settings

--- custom_data/converters.py
+++ custom_data/converters.py
@@ -2,6 +2,14 @@ import asyncio
 import aiohttp
 import pymongo
 import json
+import logging
+
+class Settings:
+    @classmethod
+    def get_config_default(cls) -> dict:
+        return {
+
+        }
 
 
 class MongoDatabase:
@@ -14,12 +22,68 @@ class MongoDatabase:
         usr = self.config.get('username', 'root')
         pas = self.config.get('password', 'root')
         host = f"mongodb://{usr}:{pas}@{addr}:{port}/"
-        self.client = pymongo.MongoClient(host)
 
+        self.client = pymongo.MongoClient(host)
         system_info = self.client.server_info()
         version = system_info['version']
         git_ver = system_info['gitVersion']
         print(f"Connected to {addr}:{port} from {self.client.HOST}, Version: {version}, Git Version: {git_ver}")
+
+        self.db = self.client["TheInnkeeperProject"]
+        self.user_spells = self.db["TheInnkeeper-Spells"]
+        self.guild_configs = self.db["TheInnkeeper-Guilds"]
+
+    def close_conn(self):
+        self.db.logout()
+
+    # Guild settings
+    def set_guild_config(self, guild_id: int, config: dict) -> [dict, int]:
+        current_data = self.guild_configs.find_one({'_id': guild_id})
+        logging.log(
+            logging.DEBUG, "SET-GUILD: Guild with Id: {} returned with results: {}".format(guild_id, current_data))
+
+        if current_data is not None:
+            QUERY = {'_id': guild_id}
+            new_data = {'config': config}
+            resp = self.guild_configs.update_one(QUERY, {'$set': new_data})
+            logging.log(logging.INFO, "Data updated area with GuildId: {},\n"
+                                      "      resp: {}\n".format(guild_id, resp.raw_result))
+            return resp.raw_result
+        else:
+            data = {'_id': guild_id, 'config': config}
+            resp = self.guild_configs.insert_one(data)
+            logging.log(logging.INFO, "Data inserted into area with GuildId: {},\n"
+                                      "      resp: {}\n".format(guild_id, {'_id': resp.inserted_id,
+                                                                           'complete': resp.acknowledged}))
+            return resp.inserted_id
+
+    def get_guild_config(self, guild_id: int) -> dict:
+        current_data = self.guild_configs.find_one({'_id': guild_id})
+        logging.log(
+            logging.DEBUG, "GET-GUILD:  Guild with Id: {} returned with results: {}".format(guild_id, current_data))
+        return current_data if current_data is not None else Settings.get_config_default()
+
+    # Spells
+    def add_user_spells(self, user_id: int, name: str, url: str) -> [dict, int]:
+        current_data = self.user_spells.find_one({'_id': user_id})
+        logging.log(logging.DEBUG, "User with Id: {} returned with results: {}".format(user_id, current_data))
+        if current_data is not None:
+            urls = current_data['urls']
+            urls.append({'name': name, 'url': url})
+            resp = self.user_spells.update_one({'_id': user_id}, {'$set': {'urls': urls}})
+            logging.log(logging.INFO, "Data updated area with UserId: {},\n"
+                                      "      resp: {}\n".format(user_id, resp.raw_result))
+            return resp.raw_result
+        else:
+            data = {'_id': user_id, 'urls': [{'name': name, 'url': url}]}
+            resp = self.user_spells.insert_one(data)
+            logging.log(logging.INFO, "Data inserted into area with UserId: {},\n"
+                                      "      resp: {}\n".format(user_id, {'_id': resp.inserted_id, 'complete': resp.acknowledged}))
+            return resp.inserted_id
+
+
+
+
 
 
 class CustomSpells:
@@ -29,4 +93,7 @@ class CustomSpells:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     db = MongoDatabase()
+    print(db.add_user_spells(12345, "https://helpme.com/", "fireball"))
+
