ead9f1a47856cb2d66115ef935d33ff3e2b76500
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-05-23 15:37:46 +0100

    Re arranged code to make it less ass

--- custom_data/database.py
+++ custom_data/database.py
@@ -20,70 +20,13 @@ class Settings:
         }
 
 
-class MongoDatabase:
-    with open(r'config.json', 'r') as file:
-        config = json.load(file)
-
-    def __init__(self):
-        addr = self.config.get('host_address', 'localhost')
-        port = self.config.get('port', '27017')
-        usr = self.config.get('username', 'root')
-        pas = self.config.get('password', 'root')
-        host = f"mongodb://{usr}:{pas}@{addr}:{port}/"
-
-        self.client = pymongo.MongoClient(host)
-        system_info = self.client.server_info()
-        version = system_info['version']
-        git_ver = system_info['gitVersion']
-        print(f"Connected to {addr}:{port} from {self.client.HOST}, Version: {version}, Git Version: {git_ver}")
-
-        self.db = self.client["TheInnkeeperProject"]
-
-        # custom content
-        self.user_spells = self.db["TheInnkeeper-Spells"]
-        self.user_races = self.db["TheInnkeeper-Races"]
-        self.user_monsters = self.db["TheInnkeeper-Monsters"]
-
-        # custom guild configs
-        self.guild_configs = self.db["TheInnkeeper-Guilds"]
-
-    def close_conn(self):
-        self.db.logout()
-
-    """ Custom Guild settings """
-    def set_guild_config(self, guild_id: int, config: dict) -> [dict, int]:
-        current_data = self.guild_configs.find_one({'_id': guild_id})
-        logging.log(
-            logging.DEBUG, "SET-GUILD: Guild with Id: {} returned with results: {}".format(guild_id, current_data))
-
-        if current_data is not None:
-            QUERY = {'_id': guild_id}
-            new_data = {'config': config}
-            resp = self.guild_configs.update_one(QUERY, {'$set': new_data})
-            logging.log(logging.INFO, "Data updated area with GuildId: {},\n"
-                                      "      resp: {}\n".format(guild_id, resp.raw_result))
-            return resp.raw_result
-        else:
-            data = {'_id': guild_id, 'config': config}
-            resp = self.guild_configs.insert_one(data)
-            logging.log(logging.INFO, "Data inserted into area with GuildId: {},\n"
-                                      "      resp: {}\n".format(guild_id, {'_id': resp.inserted_id,
-                                                                           'complete': resp.acknowledged}))
-            return resp.inserted_id
-
-    def reset_guild_config(self, guild_id: int):
-        current_data = self.guild_configs.find_one_and_delete({'_id': guild_id})
-        logging.log(
-            logging.DEBUG, "DELETE-GUILD: Guild with Id: {} returned with results: {}".format(guild_id, current_data))
-        return "COMPLETE"
-
-    def get_guild_config(self, guild_id: int) -> dict:
-        current_data = self.guild_configs.find_one({'_id': guild_id})
-        logging.log(
-            logging.DEBUG, "GET-GUILD:  Guild with Id: {} returned with results: {}".format(guild_id, current_data))
-        return current_data['config'] if current_data is not None else Settings.get_config_default()
-
+class SpellsDoc:
     """ User's custom spells url storage and monitoring """
+
+    def __init__(self, db):
+        self.db = db
+        self.user_spells = self.db["TheInnkeeper-Spells"]
+
     def add_user_spells(self, user_id: int, url: str) -> [dict, int]:
         current_data = self.user_spells.find_one({'_id': user_id})
         logging.log(logging.DEBUG, "ADD-SPELLS: User with Id: {} returned with results: {}".format(
@@ -123,8 +66,15 @@ class MongoDatabase:
             "DELETE-ALL-SPELLS: User with Id: {} returned with results: {}".format(user_id, current_data))
         return "COMPLETE"
 
+
+class RacesDoc:
     """ Custom Races """
-    def add_user_races(self, user_id: int, name: str, url: str) -> [dict, int]:
+
+    def __init__(self, db):
+        self.db = db
+        self.user_races = self.db["TheInnkeeper-Races"]
+
+    def add_user_races(self, user_id: int, url: str) -> [dict, int]:
         current_data = self.user_races.find_one({'_id': user_id})
         logging.log(logging.DEBUG, "ADD-RACES: User with Id: {} returned with results: {}".format(
             user_id, current_data))
@@ -144,27 +94,16 @@ class MongoDatabase:
                                                                  'complete': resp.acknowledged}))
             return resp.inserted_id
 
-    def remove_user_races(self, user_id: int, url: str):
-        def check(value):
-            return not value['url'] == url
-
-        current_data = self.user_races.find_one({'_id': user_id})
-        logging.log(logging.DEBUG,
-                    "REMOVE-RACES: User with Id: {} returned with results: {}".format(user_id, current_data))
-        if current_data is not None:
-            urls = current_data['urls']
-            urls = list(filter(check, urls))
-            resp = self.user_races.update_one({'_id': user_id}, {'$set': {'urls': urls}})
-            logging.log(logging.INFO, "Data updated area with UserId: {},\n"
-                                      "      resp: {}\n".format(user_id, resp.raw_result))
-            return resp.raw_result
-        else:
-            return "NO-SPELLS"
+    def update_user_races(self, user_id: int, spells_urls: list) -> [dict, str]:
+        resp = self.user_races.find_one_and_update({"_id": user_id}, {'$set': {'urls': spells_urls}})
+        logging.log(logging.INFO, "Data updated area with UserId: {},\n"
+                                  "      resp: {}\n".format(user_id, resp.raw_result))
+        return resp.raw_result
 
     def get_user_races(self, user_id: int) -> [dict, None]:
         current_data = self.user_races.find_one({'_id': user_id})
         logging.log(logging.DEBUG, "GET-RACES: User with Id: {} returned with results: {}".format(user_id,
-                                                                                                  current_data))
+                                                                                                   current_data))
         return current_data
 
     def reset_all_user_races(self, user_id: int):
@@ -174,20 +113,27 @@ class MongoDatabase:
             "DELETE-ALL-RACES: User with Id: {} returned with results: {}".format(user_id, current_data))
         return "COMPLETE"
 
+
+class MonsterDoc:
     """ Custom Monsters """
-    def add_user_monsters(self, user_id: int, name: str, url: str) -> [dict, int]:
+
+    def __init__(self, db):
+        self.db = db
+        self.user_monsters = self.db["TheInnkeeper-Monsters"]
+
+    def add_user_monsters(self, user_id: int, url: str) -> [dict, int]:
         current_data = self.user_monsters.find_one({'_id': user_id})
-        logging.log(logging.DEBUG, "ADD-MONSTER: User with Id: {} returned with results: {}".format(
+        logging.log(logging.DEBUG, "ADD-MONSTERS: User with Id: {} returned with results: {}".format(
             user_id, current_data))
         if current_data is not None:
             urls = current_data['urls']
-            urls.append({'name': name, 'url': url})
+            urls.append({'url': url})
             resp = self.user_monsters.update_one({'_id': user_id}, {'$set': {'urls': urls}})
             logging.log(logging.INFO, "Data updated area with UserId: {},\n"
                                       "      resp: {}\n".format(user_id, resp.raw_result))
             return resp.raw_result
         else:
-            data = {'_id': user_id, 'urls': [{'name': name, 'url': url}]}
+            data = {'_id': user_id, 'urls': [{'url': url}]}
             resp = self.user_monsters.insert_one(data)
             logging.log(logging.INFO, "Data inserted into area with UserId: {},\n"
                                       "      resp: {}\n".format(user_id,
@@ -195,27 +141,16 @@ class MongoDatabase:
                                                                  'complete': resp.acknowledged}))
             return resp.inserted_id
 
-    def remove_user_monsters(self, user_id: int, name: str):
-        def check(value):
-            return not value['name'] == name
-
-        current_data = self.user_monsters.find_one({'_id': user_id})
-        logging.log(logging.DEBUG,
-                    "REMOVE-MONSTER: User with Id: {} returned with results: {}".format(user_id, current_data))
-        if current_data is not None:
-            urls = current_data['urls']
-            urls = list(filter(check, urls))
-            resp = self.user_monsters.update_one({'_id': user_id}, {'$set': {'urls': urls}})
-            logging.log(logging.INFO, "Data updated area with UserId: {},\n"
-                                      "      resp: {}\n".format(user_id, resp.raw_result))
-            return resp.raw_result
-        else:
-            return "NO-SPELLS"
+    def update_user_monsters(self, user_id: int, spells_urls: list) -> [dict, str]:
+        resp = self.user_monsters.find_one_and_update({"_id": user_id}, {'$set': {'urls': spells_urls}})
+        logging.log(logging.INFO, "Data updated area with UserId: {},\n"
+                                  "      resp: {}\n".format(user_id, resp.raw_result))
+        return resp.raw_result
 
     def get_user_monsters(self, user_id: int) -> [dict, None]:
         current_data = self.user_monsters.find_one({'_id': user_id})
-        logging.log(logging.DEBUG, "GET-MONSTER: User with Id: {} returned with results: {}".format(user_id,
-                                                                                                  current_data))
+        logging.log(logging.DEBUG, "GET-MONSTERS: User with Id: {} returned with results: {}".format(user_id,
+                                                                                                   current_data))
         return current_data
 
     def reset_all_user_monsters(self, user_id: int):
@@ -224,6 +159,70 @@ class MongoDatabase:
             logging.DEBUG,
             "DELETE-ALL-MONSTERS: User with Id: {} returned with results: {}".format(user_id, current_data))
         return "COMPLETE"
+
+
+class GuildData:
+    """ Custom Guild settings """
+
+    def __init__(self, db):
+        self.db = db
+        self.guild_configs = self.db["TheInnkeeper-Guilds"]
+
+    def set_guild_config(self, guild_id: int, config: dict) -> [dict, int]:
+        current_data = self.guild_configs.find_one({'_id': guild_id})
+        logging.log(
+            logging.DEBUG, "SET-GUILD: Guild with Id: {} returned with results: {}".format(guild_id, current_data))
+
+        if current_data is not None:
+            QUERY = {'_id': guild_id}
+            new_data = {'config': config}
+            resp = self.guild_configs.update_one(QUERY, {'$set': new_data})
+            logging.log(logging.INFO, "Data updated area with GuildId: {},\n"
+                                      "      resp: {}\n".format(guild_id, resp.raw_result))
+            return resp.raw_result
+        else:
+            data = {'_id': guild_id, 'config': config}
+            resp = self.guild_configs.insert_one(data)
+            logging.log(logging.INFO, "Data inserted into area with GuildId: {},\n"
+                                      "      resp: {}\n".format(guild_id, {'_id': resp.inserted_id,
+                                                                           'complete': resp.acknowledged}))
+            return resp.inserted_id
+
+    def reset_guild_config(self, guild_id: int):
+        current_data = self.guild_configs.find_one_and_delete({'_id': guild_id})
+        logging.log(
+            logging.DEBUG, "DELETE-GUILD: Guild with Id: {} returned with results: {}".format(guild_id, current_data))
+        return "COMPLETE"
+
+    def get_guild_config(self, guild_id: int) -> dict:
+        current_data = self.guild_configs.find_one({'_id': guild_id})
+        logging.log(
+            logging.DEBUG, "GET-GUILD:  Guild with Id: {} returned with results: {}".format(guild_id, current_data))
+        return current_data['config'] if current_data is not None else Settings.get_config_default()
+
+
+class MongoDatabase(SpellsDoc, RacesDoc, MonsterDoc, GuildData):
+    with open(r'config.json', 'r') as file:
+        config = json.load(file)
+
+    def __init__(self):
+        addr = self.config.get('host_address', 'localhost')
+        port = self.config.get('port', '27017')
+        usr = self.config.get('username', 'root')
+        pas = self.config.get('password', 'root')
+        host = f"mongodb://{usr}:{pas}@{addr}:{port}/"
+
+        self.client = pymongo.MongoClient(host)
+        system_info = self.client.server_info()
+        version = system_info['version']
+        git_ver = system_info['gitVersion']
+        print(f"Connected to {addr}:{port} from {self.client.HOST}, Version: {version}, Git Version: {git_ver}")
+
+        self.db = self.client["TheInnkeeperProject"]
+        super().__init__(self.db)
+
+    def close_conn(self):
+        self.db.logout()
 
 
 class GuildConfig:
@@ -315,7 +314,7 @@ class CustomSpells:
         return self._cache_complete
 
     @property
-    def can_page(self)  -> bool:
+    def can_page(self) -> bool:
         return self._can_page
 
 
@@ -364,5 +363,6 @@ async def main():
     print(spell.spells_data_frame)
 
 if __name__ == "__main__":
+    #logging.basicConfig(level=logging.DEBUG)
     db = MongoDatabase()
     asyncio.run(main())
